8b3019d9ed74535788cfc142f811f088626dc050
CircleCI <circleci@transferwise.com>
2019-11-28 16:16:27 +0000

    Updates

---
+++ branch/DS-161-importing-components/_next/static/dbFiqK1dr14mPloK6B1yA/pages/components/Template.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[35],{"9Avt":function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Template",function(){return w("lX5y")}])}},[["9Avt",0,1]]]);
