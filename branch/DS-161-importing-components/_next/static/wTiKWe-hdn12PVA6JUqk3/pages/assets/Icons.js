65672cf8e807ca2a34b4c8b420fb32112586d141
CircleCI <circleci@transferwise.com>
2019-11-29 13:51:02 +0000

    Updates

---
+++ branch/DS-161-importing-components/_next/static/wTiKWe-hdn12PVA6JUqk3/pages/assets/Icons.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{skrA:function(n,s,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/assets/Icons",function(){return w("zdR1")}])}},[["skrA",0,1]]]);
