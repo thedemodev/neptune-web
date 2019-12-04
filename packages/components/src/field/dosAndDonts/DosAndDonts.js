175ce551a486f4cf25c8ec3ad75102d3aef6b333
Stephen Brandwood <stephen.brandwood@transferwise.com>
2019-12-04 15:16:39 +0000

    feat: use instruction list for do and donts

--- packages/components/src/field/dosAndDonts/DosAndDonts.js
+++ packages/components/src/field/dosAndDonts/DosAndDonts.js
@@ -3,26 +3,28 @@ import Types from 'prop-types';
 import CloseIcon from '@transferwise/icons/react/close-circle';
 import TickCircleIcon from '@transferwise/icons/react/tick-circle';
 
+import InstructionList from '../../instructionList';
+
 import './DosAndDonts.css';
 
-const DosAndDonts = ({ dos, donts }) => (
-  <div className="dos-and-donts">
-    {dos.map((doThis, index) => (
-      // eslint-disable-next-line react/no-array-index-key
-      <div className="item m-t-1" key={index}>
-        <TickCircleIcon className="do" />
-        <p className="m-l-2 m-b-0 text-primary">{doThis}</p>
-      </div>
-    ))}
-    {donts.map((dont, index) => (
-      // eslint-disable-next-line react/no-array-index-key
-      <div className="item m-t-1" key={index}>
-        <CloseIcon className="dont" />
-        <p className="m-l-2 m-b-0 text-primary">{dont}</p>
-      </div>
-    ))}
-  </div>
-);
+const DosAndDonts = ({ dos, donts }) => {
+  return (
+    <div className="dos-and-donts">
+      <InstructionList
+        instructions={[
+          ...dos.map(text => ({
+            icon: <TickCircleIcon className="do" />,
+            text,
+          })),
+          ...donts.map(text => ({
+            icon: <CloseIcon className="dont" />,
+            text,
+          })),
+        ]}
+      />
+    </div>
+  );
+};
 
 DosAndDonts.defaultProps = {
   dos: [],
--- packages/components/src/field/dosAndDonts/DosAndDonts.spec.js
+++ packages/components/src/field/dosAndDonts/DosAndDonts.spec.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 import DosAndDonts from './DosAndDonts';
 
 describe('DosAndDonts', () => {
   it('should render dos list only', () => {
     const dos = ['Test this component', 'With multiple dos'];
-    const component = shallow(<DosAndDonts dos={dos} />);
+    const component = mount(<DosAndDonts dos={dos} />);
 
-    expect(component.find('.do')).toHaveLength(2);
+    expect(component.find('Icon.do')).toHaveLength(2);
   });
 
   it('should render donts list only', () => {
     const donts = ['Putting things into production without testing'];
 
-    const component = shallow(<DosAndDonts donts={donts} />);
-    expect(component.find('.dont')).toHaveLength(1);
+    const component = mount(<DosAndDonts donts={donts} />);
+    expect(component.find('Icon.dont')).toHaveLength(1);
   });
 
   it('should render both lists', () => {
     const dos = ['Do this'];
     const donts = [`Don't do that`];
 
-    const component = shallow(<DosAndDonts dos={dos} donts={donts} />);
-    expect(component.find('.do')).toHaveLength(1);
-    expect(component.find('.dont')).toHaveLength(1);
-    expect(component.find('.item')).toHaveLength(2);
+    const component = mount(<DosAndDonts dos={dos} donts={donts} />);
+    expect(component.find('Icon.do')).toHaveLength(1);
+    expect(component.find('Icon.dont')).toHaveLength(1);
+    expect(component.find('.instruction')).toHaveLength(2);
   });
 });
