a783cfbae425fd4fabaf86642ebcf675281fcc35
CircleCI <circleci@transferwise.com>
2020-01-07 10:18:27 +0000

    Updates

---
+++ branch/storybook-setup/_next/static/w26MSXmSGApbJidi1EgYF/pages/components/InstructionsList.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{iLhm:function(n,o,i){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/InstructionsList",function(){return i("DtIx")}])}},[["iLhm",0,1]]]);
