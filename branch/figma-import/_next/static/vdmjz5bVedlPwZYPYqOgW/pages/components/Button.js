57223ddc8f1274904cbbf821bf728a39b7a5836c
CircleCI <circleci@transferwise.com>
2020-01-13 14:00:31 +0000

    Updates

---
+++ branch/figma-import/_next/static/vdmjz5bVedlPwZYPYqOgW/pages/components/Button.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{"0JCc":function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Button",function(){return w("rJO/")}])}},[["0JCc",0,1]]]);
