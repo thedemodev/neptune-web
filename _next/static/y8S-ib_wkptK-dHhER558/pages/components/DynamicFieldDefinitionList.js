16508ae13796c5fc3c0dce4bc7ea7624fca9072c
CircleCI <circleci@transferwise.com>
2020-02-05 14:15:28 +0000

    Updates

---
+++ _next/static/y8S-ib_wkptK-dHhER558/pages/components/DynamicFieldDefinitionList.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{gtqr:function(n,i,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/DynamicFieldDefinitionList",function(){return o("zXEp")}])}},[["gtqr",0,1]]]);
