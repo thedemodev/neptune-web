a76a12108e61aa03ac84f0cfd4bfd1d10996e825
CircleCI <circleci@transferwise.com>
2020-02-06 10:07:02 +0000

    Updates

---
+++ _next/static/su-HmG0Zi6H2-rjOd2bjE/pages/components/Chevron.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{P7rm:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Chevron",function(){return w("WQ91")}])}},[["P7rm",0,1]]]);
