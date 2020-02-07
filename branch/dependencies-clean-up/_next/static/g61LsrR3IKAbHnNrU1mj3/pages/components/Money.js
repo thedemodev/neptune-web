42f893106dcaa8431489c366790db39742348f24
CircleCI <circleci@transferwise.com>
2020-02-07 11:11:12 +0000

    Updates

---
+++ branch/dependencies-clean-up/_next/static/g61LsrR3IKAbHnNrU1mj3/pages/components/Money.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[26],{ZvJU:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Money",function(){return w("OGI5")}])}},[["ZvJU",0,1]]]);
