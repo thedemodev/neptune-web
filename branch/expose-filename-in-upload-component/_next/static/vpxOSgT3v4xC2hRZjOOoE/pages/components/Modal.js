825315a3f6932280b6ba0271b0cb2bb5aca3b49a
CircleCI <circleci@transferwise.com>
2020-02-07 10:34:09 +0000

    Updates

---
+++ branch/expose-filename-in-upload-component/_next/static/vpxOSgT3v4xC2hRZjOOoE/pages/components/Modal.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{j5hG:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Modal",function(){return w("LwEd")}])}},[["j5hG",0,1]]]);
