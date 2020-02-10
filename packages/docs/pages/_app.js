6a4f74c713db90d401fa2d7382d1db7945a20eca
Naiara Abaroa <naiara.abaroa@transferwise.com>
2020-02-10 11:54:26 +0000

    feat: replace bootstrap.min for neptune-css core

--- packages/docs/pages/_app.js
+++ packages/docs/pages/_app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import App from 'next/app';
 import Head from 'next/head';
 import Layout from '../components/Layout';
-import 'bootstrap/dist/css/bootstrap.css';
+import '@transferwise/neptune-css/dist/css/core.css';
 import 'currency-flags/dist/currency-flags.min.css';
 import '@transferwise/icons/dist/icons.min.css';
 
