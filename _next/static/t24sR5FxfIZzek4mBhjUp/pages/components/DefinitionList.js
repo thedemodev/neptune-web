1751a703ee718bb1f9f42055d283daabfed098fd
CircleCI <circleci@transferwise.com>
2020-02-18 16:22:36 +0000

    Updates

---
+++ _next/static/t24sR5FxfIZzek4mBhjUp/pages/components/DefinitionList.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{A9R1:function(n,o,i){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/DefinitionList",function(){return i("SB0l")}])}},[["A9R1",0,1]]]);
---
+++ _next/static/t24sR5FxfIZzek4mBhjUp/pages/components/Dimmer.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{kIOC:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Dimmer",function(){return w("4xqg")}])}},[["kIOC",0,1]]]);
