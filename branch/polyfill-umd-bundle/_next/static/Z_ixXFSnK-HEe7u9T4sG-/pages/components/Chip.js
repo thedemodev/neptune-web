ccf1b0fff2555cf8e364204f0d744ff9dc9f5906
CircleCI <circleci@transferwise.com>
2020-02-24 15:32:14 +0000

    Updates

---
+++ branch/polyfill-umd-bundle/_next/static/Z_ixXFSnK-HEe7u9T4sG-/pages/components/Chip.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{"LrS+":function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Chip",function(){return w("L9Bz")}])}},[["LrS+",0,1]]]);
