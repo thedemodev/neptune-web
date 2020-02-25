042db35591aadce9e883a053c60ab099aaf5b5ef
CircleCI <circleci@transferwise.com>
2020-02-25 17:28:49 +0000

    Updates

---
+++ _next/static/tZ7kB2t16p35kc9sdLcRI/pages/components/Tabs.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[42],{"5T9s":function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Tabs",function(){return w("U7Op")}])}},[["5T9s",0,1]]]);
