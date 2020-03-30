8cdc47c7652593995920e514b5a41443725d65bc
CircleCI <circleci@transferwise.com>
2020-03-30 12:22:06 +0000

    Updates

---
+++ branch/dependabot-npm_and_yarn-babel-plugin-transform-runtime-7.9.0/_next/static/SZwXLXMeP4Cbr8DkqRZe5/pages/components/Drawer.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{OFih:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Drawer",function(){return o("7+Rw")}])}},[["OFih",0,1]]]);
