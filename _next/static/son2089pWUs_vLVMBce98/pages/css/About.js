154de64431dd0584ba7b82eda955c3e3e32454b4
CircleCI <circleci@transferwise.com>
2020-04-06 07:34:24 +0000

    Updates

---
+++ _next/static/son2089pWUs_vLVMBce98/pages/css/About.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[46],{ZYKP:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/css/About",function(){return o("R1Xi")}])}},[["ZYKP",0,1]]]);
